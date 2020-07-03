0863ef9ca5900ad6d81a512cb586361425a59e99
yarl79 <yarl79@yandex.ru>
2020-07-03 19:51:41 +0300

    code style was fixed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,9 @@
 package jm.task.core.jdbc.dao;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,5 @@
 package jm.task.core.jdbc.dao;
 
-
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -79,7 +78,6 @@ public class UserDaoJDBCImpl implements UserDao {
                         result.getString(3),
                         result.getByte(4)));
             }
-
             result.close();
             stmt.close();
         } catch (SQLException e) {
